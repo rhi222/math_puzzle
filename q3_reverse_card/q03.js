04e283fd1d70968d404680dd122b37f65d8c1d75
Ryohei Nihiyama <ryouhei222@gmail.com>
2016-04-16 13:52:43 +0900

    1~N枚のカードを並べて、n番目のカードからn-1枚おきにカードを裏返す操作をした後の カードの状態を出力する

---
+++ q3_reverse_card/q03.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env jsc
+//カードの枚数初期化する
+var N = 100;
+var cards = [];
+for (i=1; i <= N ; i++) {
+	cards.push(false);
+}
+
+// N回、カードを裏返す
+for (i=2; i <= N ; i++) {
+	var j = i -1;
+	while(j < cards.length){
+		cards[j] = !cards[j];
+		j += i;
+	}
+}
+
+// 結果を出力
+print(cards);
+print (getAllIndex(cards, false));
+var reverseCardNumbers = getAllIndex(cards, false).map(function(index){
+	return index + 1;
+});
+print(reverseCardNumbers);
+function getAllIndex(arr, val){
+	var index = [];
+	var i = -1;
+	while ((i = arr.indexOf(val, i+1)) != -1){
+		index.push(i);
+	}
+	return index;
+}
+
