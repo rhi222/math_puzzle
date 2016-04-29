7a678f154f18509f6d940c7ae5a4a96dfdba23ca
Ryohei Nihiyama <ryouhei222@gmail.com>
2016-04-29 23:42:06 +0900

    組み合わせの処理を追加

--- q5_cach_payment/q05.js
+++ q5_cach_payment/q05.js
@@ -7,39 +7,91 @@ var maxCoinNum = 15;
 var totalPrice = 1000;
 */
 	countExchangeCombination : function (totalPrice, coins, maxCoinNum) {
-		console.log('first');
-		var combination;
-		coins.sort(function(a, b){ return (b - a);});
-		coins = _.rest(coins, 1);
-		console.log(coins);
-		if(coins.length === 0){
-			console.log('in if');
-			combination = this.combination;
-			combination = 222;
-			console.log(combination);
-			//console.log('return ');
-			//return combination;
-		} else{
-			console.log('else ');
-			combination = q5.countExchangeCombination(totalPrice, coins, maxCoinNum);
-			console.log('else combination');
-			console.log(combination);
-		console.log('else last');
+		var count;
+		for(var i = 1; i < maxCoinNum; i++){
+			var combArr = q5.combinations(coins);
+			combArr.forEach(function(arr){
+				console.log(arr);
+				console.log(q5.sumArray(arr));
+				console.log(totalPrice);
+				if (q5.sumArray(arr) == totalPrice) {
+					console.log('count +1');
+					count += 1;
+				}
+			});
 		}
-		console.log('return ');
-		return combination;
+		return count;
 	}
 
 	,sumArray : function(arr) {
 		var sum = 0;
+		if(Array.isArray(arr)){
 		arr.forEach(function(elm) {
 			sum += elm;
 		});
+		}
 		return sum;
+	}
+
+	,k_combinations : function(set, k) {
+	var i, j, combs, head, tailcombs;
+	
+	// There is no way to take e.g. sets of 5 elements from
+	// a set of 4.
+	if (k > set.length || k <= 0) {
+		return [];
+	}
+	
+	// K-sized set has only one K-sized subset.
+	if (k == set.length) {
+		return [set];
+	}
+	
+	// There is N 1-sized subsets in a N-sized set.
+	if (k == 1) {
+		combs = [];
+		for (i = 0; i < set.length; i++) {
+			combs.push([set[i]]);
+		}
+		return combs;
+	}
+	
+	combs = [];
+	for (i = 0; i < set.length - k + 1; i++) {
+		// head is a list that includes only our current element.
+		head = set.slice(i, i + 1);
+		// We take smaller combinations from the subsequent elements
+		tailcombs = q5.k_combinations(set.slice(i + 1), k - 1);
+		// For each (k-1)-combination we join it with the current
+		// and store it to the set of k-combinations.
+		for (j = 0; j < tailcombs.length; j++) {
+			combs.push(head.concat(tailcombs[j]));
+		}
+	}
+	return combs;
+	}
+
+		
+	,combinations : function(set) {
+	var k, i, combs, k_combs;
+	combs = [];
+	
+	// Calculate all non-empty k-combinations
+	for (k = 1; k <= set.length; k++) {
+		k_combs = q5.k_combinations(set, k);
+		for (i = 0; i < k_combs.length; i++) {
+			combs.push(k_combs[i]);
+		}
+	}
+	return combs;
 	}
 //var answer = countExchangeCombination (1000, [10, 50, 100, 500], 15);
 };
 
+//var answer = q5.countExchangeCombination (1000, [10, 50, 100, 500], 15);
+var answer = q5.countExchangeCombination (300, [10, 50, 100], 5);
+console.log('answer:');
+console.log(answer);
 module.exports = q5;
 //console.log(module);
 //console.log(module.exports);
--- spec/q05_spec.js
+++ spec/q05_spec.js
@@ -8,6 +8,10 @@ describe("q05 test", function() {
 		expect(q5.sumArray([2,3,4])).toEqual(9);
 	});
 
+	it("func getKsizedCombination", function(){
+		expect(q5.k_combinations([10, 50, 100], 2)).toEqual([[10, 50], [10, 100], [50, 100]]);
+	});
+
 	it("func countExchangeCombination", function(){
 		expect(q5.countExchangeCombination(500, [10, 50, 100], 5)).toEqual(9);
 	});
