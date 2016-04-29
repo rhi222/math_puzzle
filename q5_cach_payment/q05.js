eb48758aa60055b543e562cbf6a20f7f4688a8de
Ryohei Nihiyama <ryouhei222@gmail.com>
2016-04-29 21:52:51 +0900

    countExchangeCombination.関数の返り値を取得できるように修正

--- q5_cach_payment/q05.js
+++ q5_cach_payment/q05.js
@@ -1,15 +1,36 @@
+var _ = require('../lib/node_modules/underscore/underscore.js')
 //name space
 var q5 ={
 /*
 var coins = [10, 50, 100, 500];
 var maxCoinNum = 15;
 var totalPrice = 1000;
-var countExchangeCombination = function (totalPrice, coins, maxCoinNum) {
-	return combination;
-}
-
 */
-	sumArray : function(arr) {
+	countExchangeCombination : function (totalPrice, coins, maxCoinNum) {
+		console.log('first');
+		var combination;
+		coins.sort(function(a, b){ return (b - a);});
+		coins = _.rest(coins, 1);
+		console.log(coins);
+		if(coins.length === 0){
+			console.log('in if');
+			combination = this.combination;
+			combination = 222;
+			console.log(combination);
+			//console.log('return ');
+			//return combination;
+		} else{
+			console.log('else ');
+			combination = q5.countExchangeCombination(totalPrice, coins, maxCoinNum);
+			console.log('else combination');
+			console.log(combination);
+		console.log('else last');
+		}
+		console.log('return ');
+		return combination;
+	}
+
+	,sumArray : function(arr) {
 		var sum = 0;
 		arr.forEach(function(elm) {
 			sum += elm;
