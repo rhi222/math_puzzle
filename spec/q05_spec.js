c1e7d41851c35bfa3a5f03b3c3d802dff325f8f5
Ryohei Nihiyama <ryouhei222@gmail.com>
2016-04-29 18:44:57 +0900

    変数名を修正

--- spec/q05_spec.js
+++ spec/q05_spec.js
@@ -1,24 +1,11 @@
 describe("sumArray", function() {
-	var CashPayment = require('../q5_cach_payment/q05.js');
-	var cashPayment;
-	/*
-	 * var sumArray = function(arr) {
-		var sum = 0;
-		arr.forEach(function(elm) {
-			sum += elm;
-		});
-		return sum;
-	};
-	*/
-	console.log('CashPayment:');
-	console.log(CashPayment);
-	console.log(CashPayment.sumArray);
+	var q5 = require('../q5_cach_payment/q05.js');
+
 	beforeEach(function(){
-		//cashPayment = new CashPayment();
 	});
 
 	it("func sumArray return", function(){
-		expect(CashPayment.sumArray([2,3,4])).toEqual(9);
+		expect(q5.sumArray([2,3,4])).toEqual(9);
 	});
 });
 
